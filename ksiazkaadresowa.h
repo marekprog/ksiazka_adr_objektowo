dc0bfc6574241ce9bbc49eefe053971ee219431f
marekprog <marek.pierzchala.programista@gmail.com>
2019-04-12 17:40:57 +0200

    dodano zapis do pliku

--- ksiazkaadresowa.h
+++ ksiazkaadresowa.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include "uzytkownik.h"
+#include <fstream>
+#include <sstream>
 
 
 class KsiazkaAdresowa
@@ -10,16 +12,22 @@ class KsiazkaAdresowa
     int idZalogowanegoUzytkownika;
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
+    string nazwaPlikuZUzytkownikami;
     vector<Uzytkownik>uzytkownicy;
 
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
     bool czyIstniejeLogin(string login);
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    string konwersjaIntNaString(int liczba);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
 
 public:
     KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
+
 };
 
 #endif // KSIAZKAADRESOWA_H
